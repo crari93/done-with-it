603db96dd76102cb77414d485470948f5c298cb5
Ariel Rodríguez Boza <33108087+crari93@users.noreply.github.com>
2020-10-19 15:07:10 -0600

    Added a new component and some code
    Here I added a component to show all the item in the array. The I used props to get the item from the arrary and a function to hide that item when it's press.

--- App.js
+++ App.js
@@ -1,13 +1,22 @@
 import React, { useState } from "react";
 import Header from "./Components/Header";
+import TodoItem from "./Components/TodoItem";
 import { StyleSheet, Text, View, FlatList } from "react-native";
 
 export default function App() {
+  //we create an array with some items to display
   const [todos, setTodos] = useState([
     { text: "Buy coffee", key: "1" },
     { text: "Create app", key: "2" },
     { text: "Play on the PS4", key: "3" },
   ]);
+
+  const pressHandler = (key) => {
+    //here we hide a selected item from the list
+    setTodos((prevTodos) => {
+      return prevTodos.filter((todo) => todo.key != key);
+    });
+  };
   return (
     <View style={styles.container}>
       <Header />
@@ -16,7 +25,12 @@ export default function App() {
         <View style={styles.list}>
           <FlatList
             data={todos}
-            renderItem={({ item }) => <Text>{item.text}</Text>}
+            renderItem={({ item }) => (
+              /*here we are passing with props two variables to the TodoItem component: 
+                1: the array item
+                2: the hide function*/
+              <TodoItem item={item} pressHandler={pressHandler} />
+            )}
           />
         </View>
       </View>
